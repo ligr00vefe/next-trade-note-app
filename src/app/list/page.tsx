84dabc80e311e3c8aa09c825ffb145cb577c7c3a
ligr00vefe <ligr00vefe@naver.com>
2025-09-06 01:29:58 +0900

    List 페이지 페이지네이션 수정완료

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -13,9 +13,13 @@ export default async function ListPage({ searchParams }: IListPageProps) {
   const limit = searchParams.limit ? parseInt(searchParams.limit) : 10;
   const page = searchParams.page ? parseInt(searchParams.page) : 1;
 
+  // console.log('limit', limit);
+  // console.log('page', page);
+
   // 초기 데이터 로드 (sortBy: createdAt, sortOrder: desc로 고정)
   const TradeListData = await getTradeList({
     limit,
+    page,
     sortBy: 'createdAt',
     sortOrder: 'desc'
   });
